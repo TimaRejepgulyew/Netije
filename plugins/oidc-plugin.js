1c395071b66c3fc590cd8056184d050217c62f93
Leonid Belousov <leonid19992812strelok@Gmail.com>
2020-02-17 09:36:00 +0500

    edit configuration for oidc library

--- plugins/oidc-plugin.js
+++ plugins/oidc-plugin.js
@@ -3,10 +3,10 @@ const config = {
     userStore: new WebStorageStateStore({ store: window.localStorage }),
     authority: "http://192.168.4.99",
     client_id: "TTDoc.UI",
-    redirect_uri: "http://localhost:57331/callback",
+    redirect_uri: "http://localhost:3000/callback",
     response_type: "code",
     scope: "TTDoc.UIAPI openid profile offline_access",
-    post_logout_redirect_uri: "http://localhost:57331/"
+    post_logout_redirect_uri: "http://localhost:3000/"
 };
 
 export default new Oidc.UserManager(config);
