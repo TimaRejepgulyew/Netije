101aece2e2ad983b5a1b49d5e33fcdadb8f56c3f
Timur Rejepkuliew <test@gmail.com>
2020-03-24 18:11:01 +0500

    outgoingLetter page create

---
+++ pages/paper-work/incomming-letter copy/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <main class="container container--grid">
+    <Header :headerTitle="headerTitle"></Header>
+    <DxDataGrid
+      :show-borders="true"
+      :data-source="store"
+      :remote-operations="true"
+      :allow-column-reordering="true"
+      :allow-column-resizing="true"
+      :column-auto-width="true"
+      @editing-start="editingStart"
+      @init-new-row="initNewRow"
+    >
+      <DxGroupPanel :visible="true" />
+      <DxGrouping :auto-expand-all="false" />
+      <DxSelection mode="multiple" />
+      <DxHeaderFilter :visible="true" />
+
+      <DxColumnChooser :enabled="true" />
+      <DxColumnFixing :enabled="true" />
+
+      <DxFilterRow :visible="true" />
+
+      <DxExport
+        :enabled="true"
+        :allow-export-selected-data="true"
+        :file-name="$t('translations.menu.incommingLetter')"
+      />
+
+      <DxStateStoring :enabled="true" type="localStorage" storage-key="incommingLetter" />
+
+      <DxEditing
+        :allow-updating="true"
+        :allow-deleting="true"
+        :allow-adding="true"
+        :useIcons="true"
+        mode="form"
+      />
+
+      <DxSearchPanel
+        position="after"
+       
+        :visible="true"
+      />
+      <DxScrolling mode="virtual" />
+
+      <DxColumn data-field="dated" :caption="$t('translations.fields.dated')" data-type="date" />
+       <DxColumn data-field="created" :caption="$t('translations.fields.dated')" data-type="date" />
+       
+      <DxColumn data-field="name" :caption="$t('translations.fields.name')" data-type="string"></DxColumn>
+
+      <DxColumn data-field="inNumber" :caption="$t('translations.fields.regNumberDocument')"></DxColumn>
+      <DxColumn
+        data-field="correspondentId"
+        :caption="$t('translations.fields.subject')"
+        data-type="selectbox"
+      >
+        <DxLookup
+          :allow-clearing="true"
+          :data-source="correspondentStores"
+          value-expr="id"
+          display-expr="name"
+        />
+      </DxColumn>
+      <DxColumn
+        data-field="documentKindId"
+        :caption="$t('translations.fields.documentKindId')"
+        data-type="selectbox"
+      >
+        <DxLookup
+          :allow-clearing="true"
+          :data-source="documentKindStores"
+          value-expr="id"
+          display-expr="name"
+        />
+      </DxColumn>
+    </DxDataGrid>
+  </main>
+</template>
+<script>
+import DataSource from "devextreme/data/data_source";
+import dataApi from "~/static/dataApi";
+
+import Header from "~/components/page/page__header";
+import {
+  DxSearchPanel,
+  DxDataGrid,
+  DxColumn,
+  DxEditing,
+  DxHeaderFilter,
+  DxScrolling,
+  DxExport,
+  DxSelection,
+  DxLookup,
+  DxGrouping,
+  DxGroupPanel,
+  DxColumnChooser,
+  DxColumnFixing,
+  DxFilterRow,
+  DxStateStoring
+} from "devextreme-vue/data-grid";
+
+export default {
+  components: {
+    Header,
+    DxSearchPanel,
+    DxDataGrid,
+    DxColumn,
+    DxEditing,
+    DxHeaderFilter,
+    DxScrolling,
+    DxExport,
+    DxSelection,
+    DxLookup,
+    DxGrouping,
+    DxGroupPanel,
+    DxColumnChooser,
+    DxColumnFixing,
+    DxFilterRow,
+    DxStateStoring
+  },
+  data() {
+    return {
+      headerTitle: this.$t("translations.menu.incommingLetter"),
+      store: this.$dxStore({
+        key: "id",
+        loadUrl: dataApi.paperWork.IncommingLetter,
+        removeUrl: dataApi.paperWork.DeleteDocument
+      }),
+
+      statusStores: this.$store.getters["status/status"],
+
+      initNewRow: e => {
+        this.$router.push("/paper-work/incomming-letter/form/add");
+      },
+
+      editingStart: e => {
+        this.$router.push("/paper-work/incomming-letter/form/" + e.key);
+      },
+      correspondentStores: this.$dxStore({
+        key: "id",
+        loadUrl: dataApi.contragents.CounterPart
+      }),
+      documentKindStores: this.$dxStore({
+        key: "id",
+        loadUrl: dataApi.docFlow.DocumentKind
+      })
+    };
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+@import "~assets/themes/generated/variables.base.scss";
+@import "~assets/dx-styles.scss";
+.container {
+  display: block;
+}
+</style>
