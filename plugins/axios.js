8605a181f70c250bdb6ca523cdc83c795a599b38
Timur <klepersontimur@gmail.com>
2020-05-06 10:01:08 +0500

    modified axior error logic. added try catch

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,24 +4,32 @@ export default function ({ store, app: { $axios, i18n } }) {
 
   $axios.onError(error => {
     if (error.response.headers["content-type"] === "application/problem+json; charset=utf-8") {
-      var responseDetail = JSON.parse(error.response.data.detail);
-      var errors = [];
-      for (const key in responseDetail) {
-        if (responseDetail.hasOwnProperty(key)) {
-          responseDetail[key].forEach(element => {
-            errors.push(element);
-          });
+      try {
+        var responseDetail = JSON.parse(error.response.data.detail);
+        var errors = [];
+        for (const key in responseDetail) {
+          if (responseDetail.hasOwnProperty(key)) {
+            responseDetail[key].forEach(element => {
+              errors.push(element);
+            });
+          }
+        }
+
+        if (errors) {
+          alert(
+            `<ul>${errors
+              .map(el => {
+                return `<li class="text--error">${el}</li>`;
+              })
+              .join(" ")}</ul>`,
+            i18n.t("translations.shared.error")
+          );
         }
       }
-
-      alert(
-        `<ul>${errors
-          .map(el => {
-            return `<li class="text--error">${el}</li>`;
-          })
-          .join(" ")}</ul>`,
-        i18n.t("translations.shared.error")
-      );
+      catch (e) { 
+        alert(error.response.data.detail);
+        console.log(error.response.data)
+      }
     }
   });
 
