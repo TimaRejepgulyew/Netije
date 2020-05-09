6cd1693fa4c7700f10ba70e97b3e51cfc8005361
Timur Rejepkuliew <test@gmail.com>
2020-05-09 12:22:48 +0500

    document state to vuex

--- store/currentDocument.js
+++ store/currentDocument.js
@@ -8,10 +8,30 @@ export const state = () => ({
 
 export const getters = {
   isreadOnlyOrNotDataChanged({ isDataChanged, readOnly }) {
-    console.log(readOnly || !isDataChanged);
+    console.log(readOnly || !isDataChanged, "isreadOnlyOrNotDataChanged");
     return readOnly || !isDataChanged;
+  },
+  canRegister({ canRegister }) {
+    return canRegister;
+  },
+  canUpdate({ canUpdate }) {
+    return canUpdate;
+  },
+  isRegistered({ isRegistered }) {
+    return isRegistered;
+  },
+  isReadOnly({ isReadOnly }) {
+    return isReadOnly;
   }
 };
 export const mutations = {
-    
+  SET_DOCUMENT_STATE(state, payload) {
+    for (let item in payload) {
+      state[item] = payload[item];
+      console.log(state[item]);
+    }
+  },
+  DATA_CHANGED(state, payload) {
+    state.isDataChanged = payload;
+  }
 };
