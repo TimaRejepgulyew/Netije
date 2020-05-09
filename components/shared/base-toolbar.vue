7517e15b09166344458c74416cee53f9b1143a4d
Kleperson Timur <t.kleperson@turkmen-tranzit.com>
2020-05-09 11:57:14 +0500

    create document kind refactored

---
+++ components/shared/base-toolbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <DxToolbar class="toolbar">
+    <DxItem :options="backButtonOptions" location="before" widget="dxButton" />
+    <DxItem :options="saveButtonOptions" location="before" widget="dxButton" />
+  </DxToolbar>
+</template>
+<script>
+import  {
+  DxButtonItem,
+} from "devextreme-vue/form";
+import DxToolbar, { DxItem } from "devextreme-vue/toolbar";
+export default {
+  components: {
+    DxToolbar,
+    DxItem
+  },
+  props: ["canSave"],
+  data() {
+    return {
+      backButtonOptions: {
+        type: "normal",
+        icon: "back",
+        text: this.$t("shared.back"),
+        onClick: () => {
+          this.$router.go(-1);
+        }
+      }
+    };
+  },
+  computed: {
+    saveButtonOptions() {
+      return {
+        icon: "save",
+        type: "success",
+        visible: this.canSave,
+        useSubmitBehavior: true,
+        onClick: e => {
+          this.$emit("saveChanges");
+        }
+      };
+    }
+  }
+};
+</script>
+<style scoped>
+.toolbar {
+  background-color: transparent;
+  margin-bottom: 10px;
+}
+</style>
--- pages/docFlow/document-kind/create.vue
+++ pages/docFlow/document-kind/create.vue
@@ -2,127 +2,111 @@
   <div>
     <div>
       <Header :headerTitle="$t('translations.headers.addDocumentKind')"></Header>
-      <form @submit="handleSubmit">
-        <DxForm
-          :col-count="1"
-          :form-data.sync="documentKind"
-          :read-only="false"
-          :show-colon-after-label="true"
-        >
-          <DxGroupItem :col-count="1">
-            <DxSimpleItem data-field="code" data-type="string">
-               <DxRequiredRule :message="$t('translations.fields.codeRequired')" />
-              <DxLabel location="top" :text="$t('translations.fields.code')" />
-              <DxPatternRule
-                :ignore-empty-value="false"
-                :pattern="codePattern"
-                :message="$t('validation.valueMustNotContainsSpaces')"
-              />
-            </DxSimpleItem>
+      <toolbar @saveChanges="handleSubmit" :canSave="true" />
+      <DxForm
+        ref="form"
+        :col-count="1"
+        :form-data.sync="documentKind"
+        :read-only="false"
+        :show-colon-after-label="true"
+      >
+        <DxGroupItem :col-count="1">
+          <DxSimpleItem data-field="code" data-type="string">
+            <DxRequiredRule :message="$t('translations.fields.codeRequired')" />
+            <DxLabel location="top" :text="$t('translations.fields.code')" />
+            <DxPatternRule
+              :ignore-empty-value="false"
+              :pattern="codePattern"
+              :message="$t('validation.valueMustNotContainsSpaces')"
+            />
+          </DxSimpleItem>
 
-            <DxSimpleItem data-field="name">
-              <DxLabel location="top" :text="$t('translations.fields.name')" />
-              <DxRequiredRule :message="$t('translations.fields.nameRequired')" />
-            </DxSimpleItem>
+          <DxSimpleItem data-field="name">
+            <DxLabel location="top" :text="$t('translations.fields.name')" />
+            <DxRequiredRule :message="$t('translations.fields.nameRequired')" />
+          </DxSimpleItem>
 
-            <DxSimpleItem data-field="shortName">
-              <DxLabel location="top" :text="$t('translations.fields.shortName')" />
-              <DxRequiredRule :message="$t('translations.fields.shortNameRequired')" />
-            </DxSimpleItem>
+          <DxSimpleItem data-field="shortName">
+            <DxLabel location="top" :text="$t('translations.fields.shortName')" />
+            <DxRequiredRule :message="$t('translations.fields.shortNameRequired')" />
+          </DxSimpleItem>
 
-            <DxSimpleItem
-              data-field="documentFlow"
-              :editor-options="documentFlowOptions"
-              editor-type="dxSelectBox"
-            >
-              <DxLabel location="top" :text="$t('translations.fields.documentFlow')" />
-              <DxRequiredRule :message="$t('translations.fields.documentFlowRequired')" />
-            </DxSimpleItem>
+          <DxSimpleItem
+            data-field="documentFlow"
+            :editor-options="documentFlowOptions"
+            editor-type="dxSelectBox"
+          >
+            <DxLabel location="top" :text="$t('translations.fields.documentFlow')" />
+            <DxRequiredRule :message="$t('translations.fields.documentFlowRequired')" />
+          </DxSimpleItem>
 
-            <DxSimpleItem
-              data-field="numberingType"
-              :editor-options="numberingTypeOptions"
-              editor-type="dxSelectBox"
-            >
-              <DxLabel location="top" :text="$t('translations.fields.numberingType')" />
-              <DxRequiredRule :message="$t('translations.fields.numberingTypeRequired')" />
-            </DxSimpleItem>
+          <DxSimpleItem
+            data-field="numberingType"
+            :editor-options="numberingTypeOptions"
+            editor-type="dxSelectBox"
+          >
+            <DxLabel location="top" :text="$t('translations.fields.numberingType')" />
+            <DxRequiredRule :message="$t('translations.fields.numberingTypeRequired')" />
+          </DxSimpleItem>
 
-            <DxSimpleItem
-              data-field="documentTypeId"
-              :editor-options=" docTypeOptions"
-              editor-type="dxSelectBox"
-            >
-              <DxLabel location="top" :text="$t('menu.documentType')" />
-              <DxRequiredRule :message="$t('translations.fields.documentTypeIdRequired')" />
-            </DxSimpleItem>
+          <DxSimpleItem
+            data-field="documentTypeId"
+            :editor-options=" docTypeOptions"
+            editor-type="dxSelectBox"
+          >
+            <DxLabel location="top" :text="$t('menu.documentType')" />
+            <DxRequiredRule :message="$t('translations.fields.documentTypeIdRequired')" />
+          </DxSimpleItem>
 
-            <DxSimpleItem
-              :editor-options="tagboxOptions"
-              editor-type="dxTagBox"
-              data-field="availableActions"
-            >
-              <DxLabel location="top" :text="$t('translations.fields.availableActions')" />
-            </DxSimpleItem>
-            <DxSimpleItem
-              data-field="status"
-              :editor-options="statusOptions"
-              editor-type="dxSelectBox"
-            >
-              <DxLabel location="top" :text="$t('translations.fields.status')" />
-            </DxSimpleItem>
-            <DxSimpleItem data-field="generateDocumentName" editor-type="dxCheckBox">
-              <DxLabel
-                location="left"
-                alignment="left"
-                :text="$t('translations.fields.generateDocumentName')"
-              />
-            </DxSimpleItem>
+          <DxSimpleItem
+            :editor-options="tagboxOptions"
+            editor-type="dxTagBox"
+            data-field="availableActions"
+          >
+            <DxLabel location="top" :text="$t('translations.fields.availableActions')" />
+          </DxSimpleItem>
+          <DxSimpleItem
+            data-field="status"
+            :editor-options="statusOptions"
+            editor-type="dxSelectBox"
+          >
+            <DxLabel location="top" :text="$t('translations.fields.status')" />
+          </DxSimpleItem>
+          <DxSimpleItem data-field="generateDocumentName" editor-type="dxCheckBox">
+            <DxLabel
+              location="left"
+              alignment="left"
+              :text="$t('translations.fields.generateDocumentName')"
+            />
+          </DxSimpleItem>
 
-            <DxSimpleItem data-field="isDefault" editor-type="dxCheckBox">
-              <DxLabel
-                location="left"
-                alignment="left"
-                :text="$t('translations.fields.isDefault')"
-              />
-            </DxSimpleItem>
+          <DxSimpleItem data-field="isDefault" editor-type="dxCheckBox">
+            <DxLabel location="left" alignment="left" :text="$t('translations.fields.isDefault')" />
+          </DxSimpleItem>
 
-            <DxSimpleItem v-if="isNumerable" data-field="autoNumbering" editor-type="dxCheckBox">
-              <DxLabel
-                location="left"
-                alignment="left"
-                :text="$t('translations.fields.autoNumbering')"
-              />
-            </DxSimpleItem>
+          <DxSimpleItem v-if="isNumerable" data-field="autoNumbering" editor-type="dxCheckBox">
+            <DxLabel
+              location="left"
+              alignment="left"
+              :text="$t('translations.fields.autoNumbering')"
+            />
+          </DxSimpleItem>
 
-            <DxSimpleItem
-              data-field="note"
-              :col-span="1"
-              :editor-options="{height: 90}"
-              editor-type="dxTextArea"
-            >
-              <DxLabel location="top" :text="$t('translations.fields.note')" />
-            </DxSimpleItem>
-
-            <DxGroupItem :col-span="1" :col-count="12">
-              <DxButtonItem
-                :button-options="addButtonOptions"
-                horizontal-alignment="left"
-                :col-span="1"
-              />
-              <DxButtonItem
-                :col-span="11"
-                :button-options="cancelButtonOptions"
-                horizontal-alignment="left"
-              />
-            </DxGroupItem>
-          </DxGroupItem>
-        </DxForm>
-      </form>
+          <DxSimpleItem
+            data-field="note"
+            :col-span="1"
+            :editor-options="{height: 90}"
+            editor-type="dxTextArea"
+          >
+            <DxLabel location="top" :text="$t('translations.fields.note')" />
+          </DxSimpleItem>
+        </DxGroupItem>
+      </DxForm>
     </div>
   </div>
 </template>
 <script>
+import Toolbar from "~/components/shared/base-toolbar.vue";
 import "devextreme-vue/text-area";
 import Status from "~/infrastructure/constants/status";
 import NumberingType from "~/infrastructure/constants/numberingTypes";
@@ -138,7 +122,7 @@ import DxForm, {
   DxCompareRule,
   DxRangeRule,
   DxStringLengthRule,
-  DxPatternRule,
+  DxPatternRule
 } from "devextreme-vue/form";
 import dataApi from "~/static/dataApi";
 
@@ -153,7 +137,8 @@ export default {
     DxCompareRule,
     DxPatternRule,
     DxRangeRule,
-    DxForm
+    DxForm,
+    Toolbar
   },
   data() {
     return {
@@ -192,7 +177,10 @@ export default {
     goBack() {
       this.$router.go(-1);
     },
-    handleSubmit(e) {
+    handleSubmit() {
+      var res = this.$refs["form"].instance.validate();
+      if(!res.isValid)
+        return;
       this.$awn.asyncBlock(
         this.$axios.post(dataApi.docFlow.DocumentKind, this.documentKind),
         res => {
@@ -201,7 +189,6 @@ export default {
         },
         err => this.$awn.alert()
       );
-      e.preventDefault();
     }
   },
   watch: {
