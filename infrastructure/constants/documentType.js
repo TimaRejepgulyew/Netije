c611f113c09f62eddc7d2ef1e5d937d6f2af3b01
Kleperson Timur <t.kleperson@turkmen-tranzit.com>
2020-06-06 15:57:35 +0500

    some changes

--- infrastructure/constants/documentType.js
+++ infrastructure/constants/documentType.js
@@ -1,4 +1,8 @@
-export default {
+import entityTypes from "./entityTypes"
+
+export default DocumentTypeGuid;
+
+const DocumentTypeGuid = {
   IncommingDocument: 1,
   OutgoingDocument: 2,
   Order: 3,
@@ -6,5 +10,20 @@ export default {
   SimpleDocument: 5,
   Addendum: 6,
   Memo: 7,
-  PowerOfAttorney:8
-};
+  PowerOfAttorney: 8,
+  IncomingInvoice: 9,
+  ContractStatement: 10,
+  IncomingTaxInvoice: 11,
+  OutgoingTaxInvoice: 12,
+  UniversalTransferDocument: 13,
+  Waybill: 14,
+  Contract: 15,
+  SupAgreement: 16
+}
+
+export const mapToEntityType = function (documentTypeGuid) {
+  switch (documentTypeGuid) {
+    case DocumentTypeGuid.IncommingDocument:
+      return entityTypes.IncomingLetter;
+  }
+}
