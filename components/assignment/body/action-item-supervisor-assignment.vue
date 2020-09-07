021f17ac9ce462a44c7addaa95da58f0fcd93083
Leonid Belousov <leonid19992812strelok@Gmail.com>
2020-09-07 19:37:38 +0500

    required true in action supervisor assignment

--- components/assignment/body/action-item-supervisor-assignment.vue
+++ components/assignment/body/action-item-supervisor-assignment.vue
@@ -51,7 +51,7 @@ export default {
       ].newDeadline,
       // validationMessage: $t("assignment.validation.actionItemRequired"),
       placeholder: this.$t("assignment.body.actionItemSupervisorAssignment"),
-      isRequired: false,
+      isRequired: true,
       todayButton: {
         text: "Today",
         onClick: () => {
--- lang/i18n-translation/ru/documentModule.js
+++ lang/i18n-translation/ru/documentModule.js
@@ -29,7 +29,7 @@ export default {
   externalApprovalState: "Согл. с контрагентом",
   executionState: "Исполнение",
   controlExecutionState: "Контроль исполнения",
-  type: { ...DocumentType },
+  type: DocumentType,
   tabs: {
     main: "Свойства",
     relations: "Связи",
