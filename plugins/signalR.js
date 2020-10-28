d3894c137154030a1d02ca86f160a22d1747ef33
Timur <klepersontimur@gmail.com>
2020-10-28 09:58:25 +0500

    modified signalR

--- plugins/signalR.js
+++ plugins/signalR.js
@@ -3,22 +3,22 @@ import dataApi from "~/static/dataApi";
 
 export default ({ app }, inject) => {
   function connectAll() {
-    // const connection = new HubConnectionBuilder()
-    //   .withUrl(dataApi.hubs.assignmentHub, {
-    //     transport: HttpTransportType.LongPolling,
-    //     accessTokenFactory: () => app.store.getters["oidc/oidcAccessToken"]
-    //   })
-    //   .withAutomaticReconnect()
-    //   .configureLogging(LogLevel.Information)
-    //   .build()
+    const connection = new HubConnectionBuilder()
+      .withUrl(dataApi.hubs.assignmentHub, {
+        transport: HttpTransportType.WebSockets | HttpTransportType.LongPolling,
+        accessTokenFactory: () => app.store.getters["oidc/oidcAccessToken"]
+      })
+      .withAutomaticReconnect()
+      .configureLogging(LogLevel.Information)
+      .build()
 
-    // connection.on("AssignmentCounterUpdated", (array) => {
-    //   app.store.commit("notificationHub/ASSIGNMENT_COUNTER_UPDATE", array)
-    // });
-    // connection.start().then((e) => {
-    // }).catch(function (err) {
-    //   return console.error(err.toString());
-    // });
+    connection.on("AssignmentCounterUpdated", (array) => {
+      app.store.commit("notificationHub/ASSIGNMENT_COUNTER_UPDATE", array)
+    });
+    connection.start().then((e) => {
+    }).catch(function (err) {
+      return console.error(err.toString());
+    });
 
 
 
