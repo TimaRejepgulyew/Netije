2c1411baddf22910c60ff0c0adc4d285fdd2faa9
Timur Rejepguliev <t.redzhepguliev@turkmen-tranzit.com>
2020-11-06 18:33:28 +0500

    expander delete free approval assignemtn

--- components/assignment/additional/free-approval-assignment.vue
+++ components/assignment/additional/free-approval-assignment.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
-    <custom-expander
-      v-if="inProcess && canUpdate"
-      :header="header"
-      bodyTemplate="approvers"
-    >
-      <approvers-list slot="approvers" :assignmentId="assignmentId" />
-    </custom-expander>
     <div v-if="false && !isRework">
       <label class="pr-2">{{ $t("assignment.readdressToEmployee") }}</label>
       <div class="f-grow-1">
@@ -22,33 +15,13 @@
 </template>
 
 <script>
-import customExpander from "~/components/custom-expander/index.vue";
-import approversList from "~/components/assignment/form-components/approvers-list.vue";
 import employeeSelectBox from "~/components/employee/custom-select-box.vue";
 export default {
   components: {
     employeeSelectBox,
-    approversList,
-    customExpander
   },
   props: ["assignmentId"],
-  methods: {
-    valueChanged(id) {
-      this.$store.commit(
-        `assignments/${this.assignmentId}/SET_ADDRESSEE_ID`,
-        id
-      );
-    }
-  },
   computed: {
-    header() {
-      return {
-        text: this.$t("assignment.fields.approvers")
-      };
-    },
-    inProcess() {
-      return this.$store.getters[`assignments/${this.assignmentId}/inProcess`];
-    },
     isRework() {
       return this.$store.getters[`assignments/${this.assignmentId}/assignment`]
         .isRework;
