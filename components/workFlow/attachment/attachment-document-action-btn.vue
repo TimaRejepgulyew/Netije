368193b237273e5abc4c7355e2058419209979a0
Timur Rejepguliev <t.redzhepguliev@turkmen-tranzit.com>
2020-11-17 13:33:36 +0500

    attaçhment document action btn

--- components/workFlow/attachment/attachment-document-action-btn.vue
+++ components/workFlow/attachment/attachment-document-action-btn.vue
@@ -1,77 +1,153 @@
 <template>
-  <DxDropDownButton
-    styling-mode="text"
-    icon="overflow"
-    :showArrowIcon="false"
-    :drop-down-options="{ width: 230 }"
-    :items="btnType"
-    display-expr="name"
-    @item-click="onItemClick"
-  />
+  <div>
+    <DxPopup
+      :visible.sync="isOpenPopup"
+      :drag-enabled="false"
+      :close-on-outside-click="true"
+      :show-title="true"
+      width="90%"
+      maxHeight="95%"
+      height="auto"
+    >
+      <div class="scrool-auto">
+        <task-card
+          @onClose="tooglePopup"
+          :taskId="taskId"
+          v-if="isOpenPopup"
+          :isCard="true"
+        />
+      </div>
+    </DxPopup>
+    <DxMenu
+      :adaptivityEnabled="true"
+      :data-source="menuOptions"
+      show-first-submenu-mode="onHover"
+      orientation="vertical"
+      submenu-direction="auto"
+      :hide-submenu-on-mouse-leave="true"
+      display-expr="text"
+      @item-click="onItemClick"
+    />
+  </div>
 </template>
 <script>
+import taskCard from "~/components/task/index.vue";
+import { DxPopup } from "devextreme-vue/popup";
+import TaskType from "~/infrastructure/constants/taskType.js";
+import sendIcon from "~/static/icons/send.svg";
+import DocumentSendAction from "~/infrastructure/models/DocumentSendAction.js";
+import DxMenu from "devextreme-vue/menu";
 import cardIcon from "~/static/icons/card.svg";
 import DocumentService from "~/infrastructure/services/documentVersionService";
 import { DxDropDownButton } from "devextreme-vue";
 import dataApi from "~/static/dataApi";
 export default {
   components: {
-    DxDropDownButton
+    DxMenu,
+    DxPopup,
+    taskCard
   },
   props: ["attachment"],
   data() {
-    const canPreview =
-      this.attachment.entity.hasVersions &&
-      this.attachment.entity.canBeOpenedWithPreview;
-
     return {
-      btnType: [
-        {
-          type: "preview",
-          visible: canPreview,
-          icon: "pdffile",
-          name: this.$t("buttons.preview")
-        },
-        {
-          type: "download",
-          visible: this.attachment.entity.hasVersions,
-          icon: "download",
-          name: this.$t("buttons.download")
-        },
-        {
-          type: "detach",
-          visible: this.attachment.canDetach,
-          icon: "trash",
-          name: this.$t("buttons.deleteLink")
-        },
-        {
-          type: "card",
-          icon: cardIcon,
-          name: this.$t("buttons.showCard")
-        }
-      ]
+      isOpenPopup: false,
+      taskId: false
     };
   },
-  computed: {},
+  computed: {
+    menuOptions() {
+      const canPreview =
+        this.attachment.entity.hasVersions &&
+        this.attachment.entity.canBeOpenedWithPreview;
+      return [
+        {
+          icon: "overflow",
+          items: [
+            {
+              type: "preview",
+              visible: canPreview,
+              icon: "pdffile",
+              text: this.$t("buttons.preview")
+            },
+            {
+              type: "download",
+              visible: this.attachment.entity.hasVersions,
+              icon: "download",
+              text: this.$t("buttons.download")
+            },
+            {
+              type: "detach",
+              visible: this.attachment.canDetach,
+              icon: "trash",
+              text: this.$t("buttons.deleteLink")
+            },
+            {
+              type: "card",
+              icon: cardIcon,
+              text: this.$t("buttons.showCard")
+            },
+            {
+              type: "availableAction",
+              icon: sendIcon,
+              text: this.$t("buttons.send"),
+              items: this.items
+            }
+          ]
+        }
+      ];
+    },
+    items() {
+      const availableActions = [
+        ...this.attachment.entity?.availableActions,
+        TaskType.SimpleTask
+      ] || [TaskType.SimpleTask];
+      const items = Object.values(
+        new DocumentSendAction(this).init().filtering(availableActions)
+      );
+      return items;
+    }
+  },
   methods: {
     showCard() {
       this.$emit("showCard");
     },
     onItemClick(e) {
-      switch (e.itemData.type) {
-        case "preview":
-          this.previewDocument();
-          break;
-        case "download":
-          this.downloadDocument();
-          break;
-        case "detach":
-          this.detachLink();
-          break;
-        case "card":
-          this.showCard();
-          break;
-      }
+      if (!e.itemData.items)
+        switch (e.itemData.type) {
+          case "preview":
+            this.previewDocument();
+            break;
+          case "download":
+            this.downloadDocument();
+            break;
+          case "detach":
+            this.detachLink();
+            break;
+          case "card":
+            this.showCard();
+            break;
+          default:
+            this.createTask(e.itemData);
+        }
+    },
+    createTask(itemData) {
+      this.$awn.asyncBlock(
+        itemData.create(this, {
+          documentId: this.attachment.entity.id,
+          documentTypeGuid: this.attachment.entity.documentTypeGuid
+        }),
+        ({ taskType, taskId }) => {
+          this.showRelationDocument(taskId);
+        },
+        () => {}
+      );
+    },
+    showRelationDocument(taskId) {
+      this.taskId = taskId;
+      this.tooglePopup();
+    },
+    tooglePopup() {
+      this.isOpenPopup = !this.isOpenPopup;
     },
     detachLink() {
       this.$emit("detach", this.attachment.attachmentId);
