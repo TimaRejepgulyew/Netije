1522009453e5852c7cbe100fa7aab5033912a766
Timur Rejepguliev <t.redzhepguliev@turkmen-tranzit.com>
2020-12-24 17:59:31 +0500

    açtion item assignment

---
+++ components/assignment-module/form-by-type/action-item-exicution/assignment/components/description.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="d-flex align-content-center info-message"
+    v-if="!isElectronicAcquaintance"
+  >
+    <i class="dx-icon-info info-message__icon"></i>
+
+    <div class="info-message__text">
+      <div>{{ $t("shared.explanation") }}</div>
+      <div>{{ description }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["assignmentId"],
+  computed: {
+    isElectronicAcquaintance() {
+      return (
+        this.$store.getters[`assignments/${this.assignmentId}/assignment`]
+          .description === null
+      );
+    },
+    description() {
+      return this.$store.getters[`assignments/${this.assignmentId}/assignment`]
+        .description;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.info-message {
+  font-size: 1.1em;
+  font-weight: 500px;
+  font-style: italic;
+  .info-message__icon {
+    padding: 5px;
+    display: inline-flex;
+    align-self: center;
+    font-size: 25px;
+  }
+}
+</style>
