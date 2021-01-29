805989ce1dabf8dad353915de60bfd50adc1f587
Timur Rejepguliev <t.redzhepguliev@turkmen-tranzit.com>
2021-01-29 12:13:18 +0500

    process.env ===production not connection

--- infrastructure/hubs/client-notification.js
+++ infrastructure/hubs/client-notification.js
@@ -5,26 +5,27 @@ import {
 } from "@microsoft/signalr";
 export default function(app) {
   const connection = new HubConnectionBuilder()
-    .withUrl("http://192.168.4.152:8886/SignalR")
-    // .withUrl("http://localhost:8886/SignalR")
+    // .withUrl("http://192.168.4.152:8886/SignalR")
+    .withUrl("http://localhost:8886/SignalR")
     // .withAutomaticReconnect(0)
     .withAutomaticReconnect()
     .configureLogging(LogLevel.Information)
     .build();
 
   async function sendUserId(userId) {
-    console.log(userId);
-    try {
-      await connection.start();
-      const serverUrl =
-        process.env.NODE_ENV === "production"
-          ? document.location.origin
-          : process.env.serverUrl;
-      await connection.invoke("notification", userId, serverUrl);
-      stopConnection();
-      return true;
-    } catch {
-      return false;
+    if (process.env.NODE_ENV === "production") {
+      try {
+        await connection.start();
+        const serverUrl =
+          process.env.NODE_ENV === "production"
+            ? document.location.origin
+            : process.env.serverUrl;
+        await connection.invoke("notification", userId, serverUrl);
+        stopConnection();
+        return true;
+      } catch {
+        return false;
+      }
     }
   }
   return sendUserId;
