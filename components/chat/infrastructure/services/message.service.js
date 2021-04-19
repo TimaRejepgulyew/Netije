52ad85f8ba5f0f1a927f3d81739367aeaba81f17
Timur Rejepguliev <t.redzhepguliev@turkmen-tranzit.com>
2021-04-19 11:16:55 +0500

    .

--- components/chat/infrastructure/services/message.service.js
+++ components/chat/infrastructure/services/message.service.js
@@ -3,7 +3,6 @@ export default class MessageService {
     static async messages(ctx,payload){
       const {take,skip,} = payload
       const {data} = await  ctx.$axios(`${process.env.chatServerUrl}${dataApi.chat.Messages}${payload.roomId}`,{params:{take,skip}})
-      console.log(data);
       return data
     }
 }
--- components/chat/infrastructure/services/room.service.js
+++ components/chat/infrastructure/services/room.service.js
@@ -1,13 +1,11 @@
 import dataApi from "~/static/dataApi";
 export default class RoomService {
   static async allRooms(ctx) {
-    ctx.$axios.setBaseURL(process.env.chatServerUr)
     const { data } = await ctx.$axios.get(`${process.env.chatServerUrl}${dataApi.chat.Rooms}`)
     console.log(data);
     return data
   }
   static async createRoom(ctx, room) {
-    ctx.$axios.setBaseURL(process.env.chatServerUr)
     const { data } = await ctx.$axios.post(`${process.env.chatServerUrl}${dataApi.chat.CreateRoom}`, room)
     console.log(data);
     return data
