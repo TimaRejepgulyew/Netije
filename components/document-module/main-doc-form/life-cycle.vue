c6224c41f6e4985e347d811853e4b3714d46c6e3
Tima Rejepguliev <TimaRejepgulyew99>
2021-07-13 09:49:12 +0500

    document-deadline in life cycle

--- components/document-module/main-doc-form/life-cycle.vue
+++ components/document-module/main-doc-form/life-cycle.vue
@@ -65,7 +65,7 @@ import DxForm, {
   DxGroupItem,
   DxSimpleItem,
   DxRequiredRule,
-  DxLabel
+  DxLabel,
 } from "devextreme-vue/form";
 import dataApi from "~/static/dataApi";
 export default {
@@ -74,7 +74,7 @@ export default {
     DxSimpleItem,
     DxRequiredRule,
     DxLabel,
-    DxForm
+    DxForm,
   },
   props: ["documentId"],
   data() {
@@ -85,8 +85,8 @@ export default {
         internalApprovalState: false,
         externalApprovalState: false,
         executionState: false,
-        controlExecutionState: false
-      }
+        controlExecutionState: false,
+      },
     };
   },
   created() {
@@ -139,7 +139,7 @@ export default {
           this.itemVisible.externalApprovalState = true;
           break;
       }
-    }
+    },
   },
   computed: {
     canUpdate() {
@@ -155,16 +155,16 @@ export default {
       return {
         readOnly: !this.canUpdate,
         ...this.$store.getters["globalProperties/FormOptions"]({
-          context: this
+          context: this,
         }),
-        onValueChanged: e => {
+        onValueChanged: (e) => {
           this.$store.commit(
             `documents/${this.documentId}/SET_LIFE_CYCLE_STATE`,
             e.value
           );
         },
         value: this.document.lifeCycleState,
-        dataSource: generateLifeCycleItemState(this, this.documentTypeGuid)
+        dataSource: generateLifeCycleItemState(this, this.documentTypeGuid),
       };
     },
     registrationStateOptions() {
@@ -172,76 +172,76 @@ export default {
         visible: this.itemVisible.registrationState,
         readOnly: true,
         ...this.$store.getters["globalProperties/FormOptions"]({
-          context: this
+          context: this,
         }),
         value: this.document.registrationState,
-        dataSource: RegistrationStateStore(this)
+        dataSource: RegistrationStateStore(this),
       };
     },
     internalApprovalStateOptions() {
       return {
         readOnly: !this.canUpdate,
         ...this.$store.getters["globalProperties/FormOptions"]({
-          context: this
+          context: this,
         }),
-        onValueChanged: e => {
+        onValueChanged: (e) => {
           this.$store.commit(
             `documents/${this.documentId}/SET_INTERNAL_APPROVAL_STATE`,
             e.value
           );
         },
         value: this.document.internalApprovalState,
-        dataSource: InternalApprovalStateStore(this)
+        dataSource: InternalApprovalStateStore(this),
       };
     },
     externalApprovalStateOptions() {
       return {
         readOnly: !this.canUpdate,
         ...this.$store.getters["globalProperties/FormOptions"]({
-          context: this
+          context: this,
         }),
-        onValueChanged: e => {
+        onValueChanged: (e) => {
           this.$store.commit(
             `documents/${this.documentId}/SET_EXTERNAL_APPROVAL_STATE`,
             e.value
           );
         },
         value: this.document.externalApprovalState,
-        dataSource: ExternalApprovalStateStore(this)
+        dataSource: ExternalApprovalStateStore(this),
       };
     },
     executionStateOptions() {
       return {
         readOnly: !this.canUpdate,
         ...this.$store.getters["globalProperties/FormOptions"]({
-          context: this
+          context: this,
         }),
-        onValueChanged: e => {
+        onValueChanged: (e) => {
           this.$store.commit(
             `documents/${this.documentId}/SET_EXECUTION_STATE`,
             e.value
           );
         },
         value: this.document.executionState,
-        dataSource: ExecutionStateStore(this)
+        dataSource: ExecutionStateStore(this),
       };
     },
     controlExecutionStateOptions() {
       return {
         readOnly: !this.canUpdate,
         ...this.$store.getters["globalProperties/FormOptions"]({
-          context: this
+          context: this,
         }),
-        onValueChanged: e => {
+        onValueChanged: (e) => {
           this.$store.commit(
             `documents/${this.documentId}/SET_CONTROL_EXECUTION_STATE`,
             e.value
           );
         },
         value: this.document.controlExecutionState,
-        dataSource: ControlExecutionStateStore(this)
+        dataSource: ControlExecutionStateStore(this),
       };
-    }
-  }
+    },
+  },
 };
 </script>
