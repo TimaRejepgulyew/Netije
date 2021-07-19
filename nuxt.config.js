c9c27ce2b529e7f73968bbfabddecae428052b5f
Leonid Belousov <l.belousov@turkmen-tranzit.com>
2021-07-19 10:09:59 +0500

    new conf

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 export default {
 	env: {
 		oidcClientId: process.env.NODE_ENV !== 'production' ? 'TTDoc.UI' : 'TTDoc.UI-prod',
-		serverUrl: 'https://192.168.4.177',
+		serverUrl: "https://192.168.4.177",
 		chatServerUrl: 'https://192.168.4.161:4000'
 	},
 	loading: {
