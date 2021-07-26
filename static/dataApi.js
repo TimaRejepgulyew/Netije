138bb34c403c63e212be2270a6c550ea79b09717
Tima Rejepguliev <TimaRejepgulyew99>
2021-07-26 14:57:16 +0500

    add dataApi port

--- static/dataApi.js
+++ static/dataApi.js
@@ -1,4 +1,4 @@
-const chatBaseUrl = process.env.NODE_ENV === "production" ? `${document.location.origin}/chat` : process.env.chatServerUrl
+const chatBaseUrl = process.env.NODE_ENV === "production" ? `${document.location.origin}:4000/chat` : process.env.chatServerUrl
 export default {
   UserPhotoHash:
     process.env.NODE_ENV === "production"
