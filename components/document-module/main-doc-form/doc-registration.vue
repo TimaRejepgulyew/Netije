0881685498009bec6c065248285d0fcdb69c11d5
Tima Rejepguliev <TimaRejepgulyew99>
2021-08-05 12:33:03 +0500

    doc registrstion

--- components/document-module/main-doc-form/doc-registration.vue
+++ components/document-module/main-doc-form/doc-registration.vue
@@ -1,30 +1,51 @@
 <template >
   <DxForm
+    :visible="isRegistrable"
     :col-count="1"
-    :read-only="readOnly"
+    :read-only="!canUpdate"
     :show-colon-after-label="true"
     :show-validation-summary="false"
+    :validation-group="documentValidatorName"
   >
     <DxGroupItem :col-count="1">
-      <DxGroupItem :caption="$t('document.groups.captions.numberAndDate')">
-        <DxSimpleItem :data-field="$t('document.fields.registrationNumber')">
-          <DxLabel location="left" />
+      <DxGroupItem
+        :visible="numberingAndDateVisible"
+        :caption="$t('document.groups.captions.numberAndDate')"
+      >
+        <DxSimpleItem
+          data-field="registrationNumber"
+          :editor-options="registrationNumberOptions"
+        >
+          <DxLabel location="left" :text="registrationNumberHelptext" />
         </DxSimpleItem>
 
-        <DxSimpleItem data-field="documentRegister">
+        <DxSimpleItem
+          :visible="isRegistrable"
+          data-field="documentRegister"
+          :editor-options="documentRegisterOptions"
+        >
           <DxLabel
             location="left"
             :text="$t('document.fields.documentRegisterId')"
           />
         </DxSimpleItem>
 
-        <DxSimpleItem data-field="registrationDate" editor-type="dxDateBox">
+        <DxSimpleItem
+          data-field="registrationDate"
+          :editor-options="registrationDateOptions"
+          editor-type="dxDateBox"
+        >
           <DxLabel
             location="left"
             :text="$t('document.fields.registrationDate')"
           />
         </DxSimpleItem>
-        <DxSimpleItem data-field="deliveryMethodId" editor-type="dxSelectBox">
+        <DxSimpleItem
+          :visible="deliveryMethodVisible"
+          data-field="deliveryMethodId"
+          :editor-options="deliveryMethodOptions"
+          editor-type="dxSelectBox"
+        >
           <DxLabel
             location="left"
             :text="$t('document.fields.deliveryMethodId')"
@@ -32,11 +53,20 @@
         </DxSimpleItem>
       </DxGroupItem>
       <DxGroupItem :caption="$t('document.groups.captions.storing')">
-        <DxSimpleItem data-field="caseFileId" editor-type="dxSelectBox">
+        <DxSimpleItem
+          :isRequired="caseFileRequired"
+          data-field="caseFileId"
+          :editor-options="caseFileOptions"
+          editor-type="dxSelectBox"
+        >
           <DxLabel location="left" :text="$t('document.fields.caseFileId')" />
         </DxSimpleItem>
 
-        <DxSimpleItem data-field="placedToCaseFileDate" editor-type="dxDateBox">
+        <DxSimpleItem
+          data-field="placedToCaseFileDate"
+          :editor-options="placedToCaseFileDateOptions"
+          editor-type="dxDateBox"
+        >
           <DxLabel
             location="left"
             :text="$t('document.fields.placedToCaseFileDate')"
@@ -47,11 +77,14 @@
   </DxForm>
 </template>
 <script>
+import DocumentTypeGuid from "~/infrastructure/constants/documentType.js";
+import NumberingType from "~/infrastructure/constants/numberingTypes.js";
 import DxForm, {
   DxGroupItem,
   DxSimpleItem,
   DxLabel,
 } from "devextreme-vue/form";
+import dataApi from "~/static/dataApi";
 export default {
   components: {
     DxGroupItem,
@@ -59,8 +92,125 @@ export default {
     DxLabel,
     DxForm,
   },
-  props: ["readOnly"],
-
-  computed: {},
+  props: ["documentId"],
+  inject: ["documentValidatorName"],
+  computed: {
+    document() {
+      return this.$store.getters[`documents/${this.documentId}/document`];
+    },
+    isRegistrable() {
+      return this.$store.getters[`documents/${this.documentId}/isRegistrable`];
+    },
+    registrationNumberHelptext() {
+      return this.isRegistrable
+        ? this.$t("documentRegistration.registrationNumber")
+        : this.$t("documentRegistration.documentNumber");
+    },
+    deliveryMethodVisible() {
+      const documentTypeGuid = this.document.documentTypeGuid;
+      return (
+        documentTypeGuid == DocumentTypeGuid.IncomingLetter ||
+        documentTypeGuid == DocumentTypeGuid.OutgoingLetter
+      );
+    },
+    numberingAndDateVisible() {
+      return (
+        this.document.documentKind?.numberingType !==
+        NumberingType.NotNumberable
+      );
+    },
+    canRegister() {
+      return this.$store.getters[`documents/${this.documentId}/canRegister`];
+    },
+    caseFileRequired() {
+      return Boolean(this.document.placedToCaseFileDate);
+    },
+    isRegistered() {
+      return this.$store.getters[`documents/${this.documentId}/isRegistered`];
+    },
+    canUpdate() {
+      return this.$store.getters[`documents/${this.documentId}/canUpdate`];
+    },
+    readOnly() {
+      return this.$store.getters[`documents/${this.documentId}/readOnly`];
+    },
+    placedToCaseFileDateOptions() {
+      return {
+        readOnly: !this.canRegister,
+        ...this.$store.getters["globalProperties/FormOptions"]({
+          context: this,
+        }),
+        useMaskBehavior: true,
+        openOnFieldClick: true,
+        value: this.document.placedToCaseFileDate,
+        onValueChanged: (e) => {
+          this.$store.commit(
+            `documents/${this.documentId}/SET_PLACE_TO_CASE_FILE_DATE_ID`,
+            e.value
+          );
+        },
+      };
+    },
+    registrationNumberOptions() {
+      return {
+        disabled: true,
+        value: this.document.registrationNumber,
+      };
+    },
+    registrationDateOptions() {
+      return {
+        disabled: true,
+        value: this.document.registrationDate,
+      };
+    },
+    deliveryMethodOptions() {
+      return {
+        ...this.$store.getters["globalProperties/FormOptions"]({
+          context: this,
+          url: dataApi.docFlow.DeliveryMethod,
+        }),
+        useMaskBehavior: true,
+        openOnFieldClick: true,
+        value: this.document.deliveryMethodId,
+        onValueChanged: (e) => {
+          this.$store.commit(
+            `documents/${this.documentId}/SET_DELIVERY_METHOD_ID`,
+            e.value
+          );
+        },
+      };
+    },
+    documentRegisterOptions() {
+      return {
+        disabled: true,
+        value: this.document.documentRegister
+          ? this.document.documentRegister.name
+          : "",
+      };
+    },
+    caseFileOptions() {
+      return {
+        readOnly: !this.canRegister,
+        ...this.$store.getters["globalProperties/FormOptions"]({
+          context: this,
+          url: dataApi.docFlow.CaseFile.AvailableForUse,
+          filter: ["status", "=", 0],
+          value: "title",
+        }),
+        value: this.document.caseFileId,
+        onValueChanged: (e) => {
+          this.$store.commit(
+            `documents/${this.documentId}/SET_CASE_FILE_ID`,
+            e.value
+          );
+          if (!this.document.placedToCaseFileDate && this.document.caseFileId)
+            this.$store.commit(
+              `documents/${this.documentId}/SET_PLACE_TO_CASE_FILE_DATE_ID`,
+              new Date()
+            );
+        },
+      };
+    },
+  },
 };
 </script>
